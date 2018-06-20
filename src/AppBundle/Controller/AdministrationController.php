510dce0c7b8926240b8b5c2c3bcb6b9d62528bd8
cpailler <aprel.illectemn@gmail.com>
2018-06-20 22:29:12 +0200

    savestate

--- src/AppBundle/Controller/AdministrationController.php
+++ src/AppBundle/Controller/AdministrationController.php
@@ -12,19 +12,31 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+/**
+ * @route("/admin")
+ *
+ */
 
 class AdministrationController extends Controller
 
 {
-/**
- * @route("/admin", name="admin_home")
- *
- */
+    /**
+     * @route("/", name="admin_home")
+     *
+     */
     public function adminAccueilAction(Request $request)
     {
         return $this->render('Administration/AdminLayout.html.twig', array(
             'user'=>$this->getUser()
         ));
+    }
+
+    /**
+     * @route("carte/plats/", name="plats_list")
+     */
+
+    public function ListPlatsCarteAction(Request $request)
+    {
+
     }
 }
