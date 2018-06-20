b3f6db87dcdc2f4c368c4713fa2d7a5fb8e69f89
ccoralie <coraliecotte.cc@gmail.com>
2018-06-20 19:47:34 +0200

    override de la page login

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('web/build/')
     .setPublicPath('/web')
+    .addEntry('login', './assets/scss/login.scss')
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSassLoader()
