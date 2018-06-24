a315a6193bc9f1666ef3fc993261122868072337
Tiffanym <tiffany.maurath@orange.fr>
2018-06-24 17:50:33 +0200

    Intégration des images

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,21 @@ Encore
     .setOutputPath('web/build/')
     .setPublicPath('/web')
 
-    // Crée son jumeau sous ce lien : css/web/build/main.css
     .addEntry('css/main', './assets/scss/main.scss')
 
     .addEntry('css/dashboard', './assets/scss/dashboard.scss')
+
+
+
+    // Transfert des images dans build/images
+    .addEntry('images/FACEBOOK_LOGO', './assets/images/FACEBOOK_LOGO.png')
+    .addEntry('images/FOND', './assets/images/FOND.png')
+    .addEntry('images/INSTAGRAM_LOGO', './assets/images/INSTAGRAM_LOGO.png')
+    .addEntry('images/LOGO_QUINO', './assets/images/LOGO_QUINO.png')
+    .addEntry('images/LOGO_QUINO2', './assets/images/LOGO_QUINO2.png')
+    .addEntry('images/LOGO_QUINO3', './assets/images/LOGO_QUINO3.png')
+    .addEntry('images/ORNEMENT_LOGO', './assets/images/ORNEMENT_LOGO.png')
+    .addEntry('images/TIMBRE_LOGO_QUINO', './assets/images/TIMBRE_LOGO_QUINO.png')
 
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
