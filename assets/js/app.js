942708313a7b11c7a68593ae6b252ccdf544d839
Tiffanym <tiffany.maurath@orange.fr>
2018-06-25 10:54:24 +0200

    Ajout jQuery/jQueryUI/BootstrapJS et intégration du carousel terminée

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-console.log('Bienvenue sur WebPack!')
+// require jQuery
+const $ = require('jquery');
+require('jquery-ui');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+require('bootstrap-sass');
+
+$(document).ready(function () {
+    console.log("ok, JQuery fonctionne !");
+});
+
+console.log('Bienvenue sur WebPack!');
