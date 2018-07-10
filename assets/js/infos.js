a78404f26e71b60f074b5b6799ed8bc507fe453d
Tiffanym <tiffany.maurath@orange.fr>
2018-07-10 22:59:26 +0200

    Première partie colorisation de la carte

--- assets/js/infos.js
+++ assets/js/infos.js
@@ -5,11 +5,11 @@ initialize = function () {
     // Correspond au coordonnées du QUINO.
     var latLng = new google.maps.LatLng(48.5815618, 7.75703980000003);
 
-    //define the basic color of your map, plus a value for saturation and brightness
+    // Je défini mes variables couleur et autres...
     var	main_color = '#4a9271',
         brightness_value= 10;
 
-    //we define here the style of the map
+    // Je personalise ma carte
     var styles= [
         {
             //set saturation for the labels on the map
@@ -18,25 +18,9 @@ initialize = function () {
                 {saturation: -10}
             ]
         },
-        {	//poi stands for point of interest - don't show these lables on the map
+        {	//poi = point of interest = Lieux touristiques, espaces verts...
             featureType: "poi",
             elementType: "labels",
-            stylers: [
-                {visibility: "off"}
-            ]
-        },
-        {
-            //don't show highways lables on the map
-            featureType: 'road.highway',
-            elementType: 'labels',
-            stylers: [
-                {visibility: "off"}
-            ]
-        },
-        {
-            //don't show local road lables on the map
-            featureType: "road.local",
-            elementType: "labels.icon",
             stylers: [
                 {visibility: "off"}
             ]
@@ -49,104 +33,24 @@ initialize = function () {
                 {visibility: "off"}
             ]
         },
-        {
-            //don't show road lables on the map
-            featureType: "road",
-            elementType: "geometry.stroke",
-            stylers: [
-                {visibility: "off"}
-            ]
-        },
         //style different elements on the map
         {
-            featureType: "transit",
+            featureType: "poi", //verdure
             elementType: "geometry.fill",
             stylers: [
                 { hue: main_color },
                 { visibility: "on" },
                 { lightness: brightness_value },
-                { saturation: -10 }
-            ]
-        },
-        {
-            featureType: "poi",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: main_color },
-                { visibility: "on" },
-                { lightness: brightness_value },
-                { saturation: -10 }
-            ]
-        },
-        {
-            featureType: "poi.government",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: main_color },
-                { visibility: "on" },
-                { lightness: brightness_value },
-                { saturation: -10 }
-            ]
-        },
-        {
-            featureType: "poi.business",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: main_color },
-                { visibility: "on" },
-                { lightness: brightness_value },
-                { saturation: -10 }
-            ]
-        },
-        {
-            featureType: "transit",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: main_color },
-                { visibility: "on" },
-                { lightness: brightness_value },
-                { saturation: -10}
-            ]
-        },
-        {
-            featureType: "transit.station",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: main_color },
-                { visibility: "on" },
-                { lightness: brightness_value },
-                { saturation: -10 }
+                { saturation: -40 }
             ]
         },
         {
             featureType: "landscape",
             stylers: [
-                { hue: main_color },
                 { visibility: "on" },
-                { lightness: brightness_value },
                 { saturation: -10 }
             ]
 
-        },
-        {
-            featureType: "road",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: main_color },
-                { visibility: "on" },
-                { lightness: brightness_value },
-                { saturation: -10 }
-            ]
-        },
-        {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: main_color },
-                { visibility: "on" },
-                { lightness: brightness_value },
-                { saturation: -10 }
-            ]
         },
         {
             featureType: "water",
@@ -161,21 +65,21 @@ initialize = function () {
     ];
 
     var myOptions = {
-        zoom: 14,
+        zoom: 15,
         center: latLng,
         mapTypeId: google.maps.MapTypeId.TERRAIN, // Type de carte, différentes valeurs possible HYBRID, ROADMAP, SATELLITE, TERRAIN
         maxZoom: 20,
         styles: styles
     };
 
-    // Configuration de l'icône personnalisée
+    // Je configure Coco_Quino Lama personnalisée
     var COCO = {
         // Adresse de l'icône personnalisée
         url: "/build/images/COCO_QUINO.29901791.png",
-        scaledSize: new google.maps.Size(80, 100),
+        scaledSize: new google.maps.Size(70, 105),
     };
 
-    // On défini le lieu cible
+    // Je défini le lieu cible
     map = new google.maps.Map(document.getElementById('map'), myOptions);
 
 
@@ -183,19 +87,18 @@ initialize = function () {
         position: latLng,
         map: map,
         title: "QUINO",
-        icon: COCO,
-        shadow: COCO,
+        icon: COCO
     });
 
-    // On défini la bulle de texte.
-    var contentMarker = 'Suspendisse quis magna dapibus orci porta varius sed sit amet purus. Ut eu justo dictum elit malesuada facilisis. Proin ipsum ligula, feugiat sed faucibus a, sit amet mauris.'
+    // Je défini la bulle de texte.
+    var contentMarker = '<p style="color:black;">Hola ! C\'est moi Coco Quino le plus beau de tous les haricots !</p>'
 
     var infoWindow = new google.maps.InfoWindow({
         content: contentMarker,
         position: latLng
     });
 
-    // On défini quand la bulle de texte est activée.
+    // Je défini quand la bulle de texte est activée.
     google.maps.event.addListener(marker, 'click', function () {
         infoWindow.open(map, marker);
     });
