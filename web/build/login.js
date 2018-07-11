b61764f78c35d17fab43cb911650f320c977b07e
Unknown <max.garnier@live.fr>
2018-07-11 09:28:22 +0200

    Auto stash before merge of "dev" and "origin/dev"

---
+++ web/build/login.js
@@ -0,0 +1,15 @@
+webpackJsonp([12],{
+
+/***/ "./assets/scss/login.scss":
+/*!********************************!*\
+  !*** ./assets/scss/login.scss ***!
+  \********************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},["./assets/scss/login.scss"]);
